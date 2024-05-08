68b649c9e3838ece87b48653e9db6b10077b5351
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-08 18:54:12 +0700

    Pertama upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT_APP = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+const homeRouter = require('./routes/home.routes');
+
+app.use(homeRouter);
+
+app.listen(PORT_APP, ()=>{
+    console.log('server started at port '+ PORT_APP);
+});
