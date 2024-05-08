442a333570c5091be8a616481d3f69114a67afdc
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-09 00:08:55 +0700

    update directory

--- build-backend.sh
+++ build-backend.sh
@@ -1,7 +1,7 @@
 docker stop backend-apps 
 docker rm backend-apps   
 docker rmi backend-server 
-docker build -t backend-server . -f backend/
+docker build -t backend-server . -f backend/Dockerfile
 docker create -it --name backend-apps -p 3001:3001 backend-server 
 docker start backend-apps 
 
