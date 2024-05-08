3993e6bce7e801d569c9ca26bbf37ff9a550e0c5
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-08 23:06:30 +0700

    nambahin frontend

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:20.13.0-alpine3.19
-RUN mkdir -p /opt/app
-WORKDIR /opt/app
-COPY Backend/package.json .
-RUN npm install
-COPY Backend/ .
-EXPOSE 3000
-CMD [ "npm", "start"]
--- build-backend.sh
+++ build-backend.sh
@@ -1,8 +1,15 @@
-docker stop express-apps
-docker rm express-apps
+docker stop express-apps 
+docker rm express-apps   
 docker rmi express-server 
-docker build -t express-server .
-docker create -it --name express-apps -p 3000:3000 express-server
+docker build -t express-server . 
+docker create -it --name express-apps -p 3000:3000 express-server 
 docker start express-apps 
 
 
+### **Catatan Penting** ###
+#stop container express-apps
+#remove container express-apps
+#remove images express-server yang di buat di Dockerfile 
+#build ulang express-server dari file Dockerfile
+#buat container express-apps dari images express-server
+#start container express-apps 
