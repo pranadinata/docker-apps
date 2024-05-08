adbc3d0505e1cb3de2bbfd84621d7ff5ebbefba4
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-08 23:07:35 +0700

    update data

--- build-backend.sh
+++ build-backend.sh
@@ -2,7 +2,7 @@ docker stop express-apps
 docker rm express-apps   
 docker rmi express-server 
 docker build -t express-server . 
-docker create -it --name express-apps -p 3000:3000 express-server 
+docker create -it --name express-apps -p 3001:3001 express-server 
 docker start express-apps 
 
 
