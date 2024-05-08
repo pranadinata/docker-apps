24993d2ae6a84c274ee6fb20cd0ea3885188c6fa
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-08 22:02:54 +0700

    build automatic container

--- build-backend.sh
+++ build-backend.sh
@@ -1,1 +1,6 @@
-sudo docker create -it --name express-apps -p 3000:3000 express-server
+docker stop express-apps
+docker rm express-apps
+docker create -it --name express-apps -p 3000:3000 express-server
+docker start express-apps 
+
+
