bdcfc016a51485732abf90254b3fbddd1e5e1ab4
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-09 00:24:04 +0700

    add frontend dockerfile and auto build

---
+++ build-frontend.sh
@@ -0,0 +1,15 @@
+docker stop frontend-apps 
+docker rm frontend-apps   
+docker rmi frontend-server 
+docker build -t frontend-server . -f frontend/Dockerfile
+docker create -it --name frontend-apps -p 3001:3001 frontend-server 
+docker start frontend-apps 
+
+
+### **Catatan Penting** ###
+#stop container backend-apps
+#remove container backend-apps
+#remove images backend-server yang di buat di Dockerfile 
+#build ulang backend-server dari file Dockerfile
+#buat container backend-apps dari images backend-server
+#start container backend-apps 
