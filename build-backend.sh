8c539ebe12118fe86ad94af8e62dc656fe1680a0
Yesaya Pranadinata <yesayapranadinata.work@gmail.com>
2024-05-08 23:54:01 +0700

    update build-backend

--- build-backend.sh
+++ build-backend.sh
@@ -1,15 +1,15 @@
-docker stop express-apps 
-docker rm express-apps   
-docker rmi express-server 
-docker build -t express-server . 
-docker create -it --name express-apps -p 3001:3001 express-server 
-docker start express-apps 
+docker stop backend-apps 
+docker rm backend-apps   
+docker rmi backend-server 
+docker build -t backend-server . 
+docker create -it --name backend-apps -p 3001:3001 backend-server 
+docker start backend-apps 
 
 
 ### **Catatan Penting** ###
-#stop container express-apps
-#remove container express-apps
-#remove images express-server yang di buat di Dockerfile 
-#build ulang express-server dari file Dockerfile
-#buat container express-apps dari images express-server
-#start container express-apps 
+#stop container backend-apps
+#remove container backend-apps
+#remove images backend-server yang di buat di Dockerfile 
+#build ulang backend-server dari file Dockerfile
+#buat container backend-apps dari images backend-server
+#start container backend-apps 
